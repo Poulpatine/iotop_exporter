18862738b340f9b34232c212d1df6fc5f9fd1f80
Benjamin Binier <bbinier@sevenhugs.com>
2019-09-13 18:15:12 +0200

    add cmdline arguments

--- iotop_exporter.py
+++ iotop_exporter.py
@@ -1,30 +1,45 @@
+import argparse
+from logger import logging
 import time
-import psutil
+from psutil import Process, AccessDenied, process_iter
 from prometheus_client import start_http_server, Gauge
 
 
-def process_request():
-    command = 'java'
+def process_request(command):
     pidlist = []
-    for proc in psutil.process_iter():
+    for proc in process_iter():
         if proc.name() == command:
             pidlist.append(proc.pid)
 
     for pid in pidlist:
-        process = psutil.Process(pid)
-        ios = process.io_counters()
-        for iotype in ios._fields:
-            io_process.labels(io_type=iotype, pid=pid,
-                              cmd=command).set(getattr(ios, iotype))
+        process = Process(pid)
+        try:
+            ios = process.io_counters()
+            for iotype in ios._fields:
+                io_process.labels(io_type=iotype, pid=pid,
+                                  cmd=command).set(getattr(ios, iotype))
+        except AccessDenied:
+            logging.error("unable to access to PID %s stats" % pid)
     return io_process
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Launch an Iotop exporter')
+    parser.add_argument('-p',
+                        '--port',
+                        type=int,
+                        default=9099,
+                        help='default listening port of exporter')
+    parser.add_argument('-c',
+                        '--command',
+                        type=str,
+                        help='command to monitor')
+    args = parser.parse_args()
     # Start up the server to expose the metrics.
-    start_http_server(8000)
+    start_http_server(args.port)
     # Generate some requests.
     io_process = Gauge('io_process', 'Process IO',
                        ['io_type', 'pid', 'cmd'])
     while True:
-        process_request()
+        process_request(args.command)
         time.sleep(5)
